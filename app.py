f050fcb2f395a313fc49e4dcef1679af9f2629d4
dct <847038416@qq.com>
2026-08-20 23:48:37 +0800

    modify

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 """痛点收集平台 - Flask后端"""
+import webbrowser
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from openai import OpenAI
@@ -230,4 +231,6 @@ if __name__ == "__main__":
     print("  痛点收集平台已启动")
     print("  访问: http://127.0.0.1:5000")
     print("=" * 50)
+    # 自动打开浏览器
+    webbrowser.open("http://127.0.0.1:5000")
     app.run(debug=True, host="0.0.0.0", port=5000)
